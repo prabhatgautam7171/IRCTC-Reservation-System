f1abb1f805b72eec28ee34c33c0f4403da782b5c
Prabhat Gautam <112789265+prabhatgautam7171@users.noreply.github.com>
2026-05-07 22:15:44 +0530

    Disable Redis caching in trainController
    Comment out Redis caching logic for train data retrieval.

--- server/controller/trainController/trainController.js
+++ server/controller/trainController/trainController.js
@@ -8,7 +8,7 @@ import { initializeSegmentsForJourney } from '../../utils/initializeSegments.js'
 import { initializeWlQuotaForJourney } from '../../utils/initializeWlQuotaInventory.js';
 import { getDuration } from '../../utils/calculateJourneyDurationHelper.js';
 import { initializeSeatInventory } from '../../utils/initializeSeatInventory.js';
-import { redisClient } from '../../config/redis.js';
+// import { redisClient } from '../../config/redis.js';
 
 
 
@@ -644,27 +644,27 @@ export const getChoiceTrains = async (req, res) => {
     const fromValue = cleanInput(from);
     const toValue = cleanInput(to);
 
-    if (refresh === "false") {
+    // if (refresh === "false") {
 
-      console.log('Attempting to serve from cache with key:', `train:${fromValue}-${toValue}-${date}`);
-      const cacheKey = `train:${fromValue}-${toValue}-${date}`;
+    //   console.log('Attempting to serve from cache with key:', `train:${fromValue}-${toValue}-${date}`);
+    //   const cacheKey = `train:${fromValue}-${toValue}-${date}`;
 
-      console.log('⚡ Checking Redis cache ')
+    //   console.log('⚡ Checking Redis cache ')
 
-      // 🔥 1. Check Redis
-      if (redisClient) {
-        const cached = await redisClient.get(cacheKey);
+    //   // 🔥 1. Check Redis
+    //   if (redisClient) {
+    //     const cached = await redisClient.get(cacheKey);
 
-        if (cached) {
-          const parsed = JSON.parse(cached);
-          console.log("🚨 CACHED DATA:", parsed);
-          if (Array.isArray(parsed)) {
-            return res.json({ success: true, trains: parsed });
-          }
-        }
+    //     if (cached) {
+    //       const parsed = JSON.parse(cached);
+    //       console.log("🚨 CACHED DATA:", parsed);
+    //       if (Array.isArray(parsed)) {
+    //         return res.json({ success: true, trains: parsed });
+    //       }
+    //     }
 
-      }
-    }
+    //   }
+    // }
 
     const response = [];
 
@@ -1010,12 +1010,12 @@ export const getChoiceTrains = async (req, res) => {
     }
 
     // 🔥 2. Store in Redis (TTL = 2 min)
-    if (redisClient) {
-      if (response.length > 0 && refresh === "false") {
-        const cacheKey = `train:${fromValue}-${toValue}-${date}`;
-        await redisClient.setEx(cacheKey, 120, JSON.stringify(response));
-      }
-    }
+    // if (redisClient) {
+    //   if (response.length > 0 && refresh === "false") {
+    //     const cacheKey = `train:${fromValue}-${toValue}-${date}`;
+    //     await redisClient.setEx(cacheKey, 120, JSON.stringify(response));
+    //   }
+    // }
 
     console.log('refresh value:', refresh);
 
