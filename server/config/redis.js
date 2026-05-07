d5823534e72ff680bf674b3b448d97a136b98082
PRABHAT GAUTAM <prabhatgautam@192.168.1.23>
2026-05-07 17:49:59 +0530

    Handled Redis connection safely

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,10 +1,18 @@
 // config/redis.js
+
 import { createClient } from "redis";
 
 export const redisClient = createClient({
   url: "redis://127.0.0.1:6379"
 });
 
-redisClient.on("error", (err) => console.error("Redis Error", err));
+redisClient.on("error", (err) => {
+  console.log("Redis unavailable");
+});
 
-await redisClient.connect();
+try {
+  await redisClient.connect();
+  console.log("Redis Connected");
+} catch (err) {
+  console.log("Running without Redis");
+}
