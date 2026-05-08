000d32c464e3e9f53b24a3a8b8e17289f6b54bb1
Prabhat Gautam <112789265+prabhatgautam7171@users.noreply.github.com>
2026-05-08 22:16:11 +0530

    Disable Redis client configuration
    Comment out Redis client configuration and connection logic.

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,22 +1,22 @@
 //config/redis.js
 
-import { createClient } from "redis";
+// import { createClient } from "redis";
 
-export let redisClient = null;
+// export let redisClient = null;
 
-try {
-  const client = createClient({
-    url: "redis://127.0.0.1:6379"
-  });
+// try {
+//   const client = createClient({
+//     url: "redis://127.0.0.1:6379"
+//   });
 
-  client.on("error", () => {});
+//   client.on("error", () => {});
 
-  await client.connect();
+//   await client.connect();
 
-  console.log("Redis Connected");
+//   console.log("Redis Connected");
 
-  redisClient = client;
+//   redisClient = client;
 
-} catch (err) {
-  console.log("Running without Redis");
-}
+// } catch (err) {
+//   console.log("Running without Redis");
+// }
