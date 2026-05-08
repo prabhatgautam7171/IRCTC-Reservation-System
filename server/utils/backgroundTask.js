0bb9ed50b6bf762a97a67a784d83070f6fd309c7
Prabhat Gautam <112789265+prabhatgautam7171@users.noreply.github.com>
2026-05-08 23:12:07 +0530

    Add background task for email and real-time updates

---
+++ server/utils/backgroundTask.js
@@ -0,0 +1,64 @@
+import { User } from "../../model/authModel/userModel.js";
+import { generateTicket } from "../../templates/ticketTemplate.js";
+import { sendTicketEmail } from "../../utils/sendTicket.js";
+import { emitRealTimeUpdate } from "../services/booking/emitRealTimeUpdate.js";
+
+export const runBackgroundTasks = ({
+  io,
+  train,
+  journeyDate,
+  source,
+  sourceIndex,
+  destIndex,
+  selectedCoachType,
+  userId,
+  result
+}) => {
+
+  setImmediate(async () => {
+
+    try {
+
+      // =========================
+      // EMAIL
+      // =========================
+      const user = await User.findById(userId);
+
+      const html = generateTicket(result);
+
+      await sendTicketEmail({
+        to: user.email,
+        subject: "Your IRCTC e-Ticket",
+        html,
+      });
+
+      console.log("✅ Email sent");
+
+    } catch (err) {
+      console.error("❌ Email failed:", err);
+    }
+
+    try {
+
+      // =========================
+      // REALTIME UPDATE
+      // =========================
+      await emitRealTimeUpdate(
+        io,
+        train._id,
+        journeyDate,
+        source,
+        sourceIndex,
+        destIndex,
+        selectedCoachType
+      );
+
+      console.log("✅ Realtime emitted");
+
+    } catch (err) {
+      console.error("❌ Emit failed:", err);
+    }
+
+  });
+
+};
