73a9eae86dfa12ca4ac45249f87111a39e005459
Prabhat Gautam <112789265+prabhatgautam7171@users.noreply.github.com>
2026-05-08 14:00:53 +0530

    Update generateToken.js

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,6 +4,6 @@ import jwt from 'jsonwebtoken'
 
 export const generateToken = async (user, res) => {
     const token = jwt.sign({_id : user._id}, process.env.JWT_SECRET, {expiresIn:'1d'});
-    res.cookie('token', token, {httpOnly:true, sameSite:'strict', maxAge:24*60*60*1000 , secure: process.env.NODE_ENV === 'production' } );
+    res.cookie('token', token, {httpOnly:true, sameSite:'none', maxAge:24*60*60*1000 , secure:true } );
     return token;
 }
