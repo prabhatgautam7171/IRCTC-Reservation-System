ff7dfb2d68924f0fd1902137b79a8139a12b3cec
Prabhat Gautam <112789265+prabhatgautam7171@users.noreply.github.com>
2026-05-08 18:39:53 +0530

    Remove redundant logging in booking route
    Removed redundant GET route logging in bookTrainController.

--- server/controller/trainController/bookTrainController.js
+++ server/controller/trainController/bookTrainController.js
@@ -27,12 +27,6 @@ export const bookTrain = async (req, res) => {
     console.log("➡️ Params:", req.params);
     console.log("➡️ Body:", req.body);
 
-    app.get("/", (req, res) => {
-    console.log("✅ Booking route hit");
-    console.log("➡️ Params:", req.params);
-    console.log("➡️ Body:", req.body);
-    });
-
     const { passengers, source, destination, journeyDate, reachingTime, selectedQuota } = req.body;
     const io = req.app.get("io");
 
