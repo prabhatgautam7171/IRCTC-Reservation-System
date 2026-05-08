6694420fdb21410e74ffe0c8546e2c0aff33108a
Prabhat Gautam <112789265+prabhatgautam7171@users.noreply.github.com>
2026-05-08 17:28:27 +0530

    Enable credentials in CORS configuration

--- server/index.js
+++ server/index.js
@@ -52,6 +52,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL,
+    credentials: true,
     methods: ["GET", "POST"]
   }
 });
